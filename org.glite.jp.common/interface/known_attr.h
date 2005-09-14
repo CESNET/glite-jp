717f5b1c06e1db311931443e381b54aa110df60e
Jan Pospíšil <honik@ntc.zcu.cz>
2005-09-14 22:15:15 +0000

    GLITE_JP_LB_TERMINATED substituted by GLITE_JP_LB_finalStatusDate GLITE_JP_LB_SUBMITTED now in GLITE_JP_LB_lastStatusHistory added GLITE_JP_ATTR_JOBID

--- org.glite.jp.common/interface/known_attr.h
+++ org.glite.jp.common/interface/known_attr.h
@@ -7,14 +7,15 @@
 /** Job owner, as specified with RegisterJob JPPS operation */
 #define GLITE_JP_ATTR_OWNER	GLITE_JP_SYSTEM_NS ":owner" 
 
+/** JobId */
+#define GLITE_JP_ATTR_JOBID	GLITE_JP_SYSTEM_NS ":jobId" 
+
 /** Timestamp of job registration in JP.
  * Should be almost the same time as registration with LB. */
 #define GLITE_JP_ATTR_REGTIME	GLITE_JP_SYSTEM_NS ":regtime" 
 
 /** Attributes derived from LB system data
  * \see jp_job_attrs.h */
-#define GLITE_JP_LB_SUBMITTED	GLITE_JP_LB_NS ":submitted"	/**< submit time */
-#define GLITE_JP_LB_TERMINATED	GLITE_JP_LB_NS ":terminated"	/**< termination time (done, abort, cancel) */
 
 /** Namespace for LB user tags, schemaless, all values are strings */
 #define GLITE_JP_LBTAG_NS	"http://egee.cesnet.cz/en/WSDL/jp-lbtag"
