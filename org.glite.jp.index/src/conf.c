4579b7cc2d76f4a584a4b8c564827bf3af02ddb5
Miloš Mulač <mulac@civ.zcu.cz>
2005-11-30 11:35:22 +0000

    check authorization defaultly

--- org.glite.jp.index/src/conf.c
+++ org.glite.jp.index/src/conf.c
@@ -30,7 +30,7 @@ int glite_jp_get_conf(int argc, char **argv, char *config_file, glite_jp_is_conf
 	conf->port = getenv("GLITE_JPIS_PORT");
 	debug = getenv("GLITE_JPIS_DEBUG");
 	conf->debug = (debug != NULL) && (strcmp(debug, "0") != 0);
-	conf->no_auth = 1;				// check authorization
+	conf->no_auth = 0;				// check authorization
 	conf->pidfile = getenv("GLITE_JPIS_PIDFILE");
 	conf->logfile = getenv("GLITE_JPIS_LOGFILE");
 
