80878ce995df8f52b651bae89c704f4e63f407cd
Miloš Mulač <mulac@civ.zcu.cz>
2006-08-25 14:35:02 +0000

    - prototype of logging usertags

--- org.glite.jp/examples/pch06/align.sh
+++ org.glite.jp/examples/pch06/align.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+log_event() #1 - attr.name #2 attr.value
+{
+  GLITE_WMS_SEQUENCE_CODE=`$lb_logevent\
+    --jobid="$GLITE_WMS_JOBID"\
+    --source="Application"\
+    --sequence="$GLITE_WMS_SEQUENCE_CODE"\
+    --event="UserTag"\
+    --node=$host\
+    --name="$1"\
+    --value="$2"\
+  || echo $GLITE_WMS_SEQUENCE_CODE`
+}
+
+
 exec 2>$$.err >&2
 set -x
 
@@ -9,6 +23,14 @@ date
 echo $0 $*
 
 chmod +x align_warp scanheader
+
+lb_logevent=${GLITE_WMS_LOCATION}/bin/glite-lb-logevent
+if [ ! -x "$lb_logevent" ]; then
+  lb_logevent="${EDG_WL_LOCATION}/bin/edg-wl-logev"
+fi
+host=`hostname -f`
+
+log_event "IPAW_PROGRAM" "align_warp"
 
 globus-url-copy $1.img file://$PWD/anatomy.img
 globus-url-copy $1.hdr file://$PWD/anatomy.hdr
