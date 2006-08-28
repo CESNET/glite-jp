0d92f17aedc57b5471a6fe4994b4d81788c457b7
Aleš Křenek <ljocha@ics.muni.cz>
2006-08-28 11:30:09 +0000

    required env. setting

--- org.glite.jp/examples/pch06/slicer.sh
+++ org.glite.jp/examples/pch06/slicer.sh
@@ -10,6 +10,8 @@ globus-url-copy $1.hdr file://$PWD/atlas.hdr
 globus-url-copy $1.img file://$PWD/atlas.img
 
 chmod +x slicer
+FSLOUTPUTTYPE=ANALYZE
+export FSLOUTPUTTYPE
 ./slicer atlas.hdr -$2 .5 atlas-$2.pgm
 
 globus-url-copy file://$PWD/atlas-$2.pgm $1-$2.pgm
